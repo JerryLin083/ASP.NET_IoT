3894efb1fc9046e8d1a2c3ee30bdfe0aa208c75f
JerryLin083 <stabber5760@gmail.com>
2026-01-06 13:24:11 +0800

    加入專案檔案。

---
+++ ASP.NET_IOT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_IOT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
